aff3d0dcdb16dd9c61eab0b316b48887dd6250cc
dhoman01 <dustin.e.homan@gmail.com>
2016-12-07 20:37:18 -0700

    Ooops

--- im2txt/im2txt/configuration.py
+++ im2txt/im2txt/configuration.py
@@ -72,7 +72,7 @@ class ModelConfig(object):
 
     # LSTM input and output dimensionality, respectively.
     self.embedding_size = 512
-    self.rnn_size = 512
+    self.num_lstm_units = 512
 
     # If < 1.0, the dropout keep probability applied to LSTM variables.
     self.lstm_dropout_keep_prob = 0.7
